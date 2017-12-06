5c0e92452ae78c952e674fcf126015dd133bc442
Rohit Singh <rohit@yogems.com>
2017-12-06 19:42:36 +0530

    commit code

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app = express();
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
+
+app.get('/', (req, res) => {
+ // res.send('hello world')
+  res.sendFile(__dirname + '/index.html')
+})
